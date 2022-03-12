c18eb7603593c1588944efc51cab385cfc0e6484
Dongwon-tuna <61178312+Dongwon-tuna@users.noreply.github.com>
2022-03-12 11:11:43 +0900

    hub test

---
+++ src/java_study_2022/Hi_github.java
@@ -0,0 +1,11 @@
+package java_study_2022;
+
+public class Hi_github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi this is fukin Great!");
+
+	}
+
+}
