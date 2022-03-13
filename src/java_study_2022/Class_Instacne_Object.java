159b24f748f2745ff027bb327bf46a275221591f
Dongwon-tuna <61178312+Dongwon-tuna@users.noreply.github.com>
2022-03-13 17:10:23 +0900

    Update Class_Instacne_Object.java

--- src/java_study_2022/Class_Instacne_Object.java
+++ src/java_study_2022/Class_Instacne_Object.java
@@ -2,16 +2,21 @@ package java_study_2022;
 
 class Calculator{
 	int left, right;
+	static double PI = 3.14; //클래스 변수
+	static int base = 0;
 	
-	public void setOprands(int left, int right) {
+	public void setOprands(int left, int right) {//인스턴스 변수
 		this.left = left;
 		this.right = right;
 	}
 	public void sum(){
-		System.out.println(this.left + this.right);
+		System.out.println(this.left + this.right + base);
 	}
 	public void avg() {
-		System.out.println((this.left + this.right)/2);
+		System.out.println((this.left + this.right + base)/2);
+	}
+	public static void div(int left, int right) {
+		System.out.println(left/right);
 	}
 }
 
@@ -23,6 +28,13 @@ public class Class_Instacne_Object {
 		c1.setOprands(10,20);
 		c1.sum();
 		c1.avg();
+		System.out.println(c1.PI);
+		Calculator c2 = new Calculator();
+		c2.base = 10;
+		c2.setOprands(10, 20);
+		c2.sum();
+		Calculator.div(24, 6);
+		
 	}
 
 }
