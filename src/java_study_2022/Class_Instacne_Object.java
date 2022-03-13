1d9034a69125241296cb0aecb11da8f007c50255
Dongwon-tuna <61178312+Dongwon-tuna@users.noreply.github.com>
2022-03-13 22:16:13 +0900

    study

--- src/java_study_2022/Class_Instacne_Object.java
+++ src/java_study_2022/Class_Instacne_Object.java
@@ -1,7 +1,7 @@
 package java_study_2022;
 
 class Calculator{
-	int left, right;
+	int left, right;//인스턴스 변수, (클래스는 인스턴스 변수, 인스턴스 메소드 접근이 불가능하다.
 	static double PI = 3.14; //클래스 변수
 	static int base = 0;
 	
@@ -15,7 +15,7 @@ class Calculator{
 	public void avg() {
 		System.out.println((this.left + this.right + base)/2);
 	}
-	public static void div(int left, int right) {
+	public static void div(int left, int right) {//클래스 메소드
 		System.out.println(left/right);
 	}
 }
@@ -33,7 +33,7 @@ public class Class_Instacne_Object {
 		c2.base = 10;
 		c2.setOprands(10, 20);
 		c2.sum();
-		Calculator.div(24, 6);
+		Calculator.div(24, 6);//굳이 c1,c2등 생성하지 않고 바로 클래스에서 원하는 값 가져옴
 		
 	}
 
